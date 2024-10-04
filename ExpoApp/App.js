5d978920e56840619896ae668b533d4531c3e348
Andysword <andrewhuang2019@gmail.com>
2024-10-03 21:57:35 -0500

    Revert "Updated tutorial"
    This reverts commit 6c4a9a2d4df5b3aa532c1e3f8ac5f94118f79196.

--- ExpoApp/App.js
+++ ExpoApp/App.js
@@ -13,13 +13,13 @@ export default function App() {
           <ImageViewer placeholderImageSource={PlaceholderImage}/>
         </View>
         <View style={styles.footerContainer}>
-          <Button theme="primary" label="Choose a photo"></Button>
+          <Button label="Choose a photo"></Button>
           <Button label="Use this photo"></Button>
         </View>
       <StatusBar style="auto" />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
