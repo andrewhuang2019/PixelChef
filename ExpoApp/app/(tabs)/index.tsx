80a58c7026f2021aaa982445ce14a7698c63e49e
Andysword <andrewhuang2019@gmail.com>
2024-10-16 17:31:52 -0500

    Created Button.tsx
    Created buttons for the home screen. Worked on the styled the index and changed footer as well

--- ExpoApp/app/(tabs)/index.tsx
+++ ExpoApp/app/(tabs)/index.tsx
@@ -1,13 +1,20 @@
 import { Text, View, StyleSheet } from 'react-native';
-import { Link } from 'expo-router'; 
+import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
+
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+          <ImageViewer imgSource={PlaceholderImage}/>
+      </View>
+      <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" />
+          <Button label="Use this photo"/>
+      </View>
     </View>
   );
 }
@@ -19,6 +26,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+
   text: {
     color: '#fff',
   },
